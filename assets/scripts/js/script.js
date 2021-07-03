6c3288dfc28d3e467a3ab5410beaf618b3e5287e
nazmul <63272906+ItZNazmul@users.noreply.github.com>
2021-07-03 21:09:41 +0600

    Added: channel Image | Dynamic Title

--- assets/scripts/js/script.js
+++ assets/scripts/js/script.js
@@ -8,12 +8,20 @@ setInterval(() => {
 
 
 function update() {
-    $.getJSON('https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id='+ChannelID+ `&key=${apikeys} `, function(data) {
-            document.getElementById("username").innerText = data.items[0].snippet.title
-            document.getElementById("odometer").innerText = data.items[0].statistics.subscriberCount
-            document.getElementById("views").innerText = data.items[0].statistics.viewCount
-            document.getElementById("videos").innerText = data.items[0].statistics.videoCount
-            document.getElementById("pageIcon").innerText = data.items[0].snippet.thumbnails.default.medium
-    })   
+    $.getJSON('https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=' + ChannelID + `&key=${apikeys} `, function (data) {
+
+
+        document.title = data.items[0].snippet.title + " | YtStats";
+
+        document.getElementById("username").innerText = data.items[0].snippet.title
+        document.getElementById("odometer").innerText = data.items[0].statistics.subscriberCount
+        document.getElementById("views").innerText = data.items[0].statistics.viewCount
+        document.getElementById("videos").innerText = data.items[0].statistics.videoCount
+        document.getElementById("ytimage").src = data.items[0].snippet.thumbnails.default.url;
+        document.getElementById("pageIcon").src = data.items[0].snippet.thumbnails.default.url;
+
+
+
+    })
 }
 
