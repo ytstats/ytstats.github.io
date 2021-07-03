cc25cf1262deadeb52741250a21fd10820e119e1
nazmul <63272906+ItZNazmul@users.noreply.github.com>
2021-07-03 16:24:58 +0600

    Added URL System

--- assets/scripts/js/script.js
+++ assets/scripts/js/script.js
@@ -1,26 +1,19 @@
-ChannelID = "UCFclAVMvm32SoYQmOr6gxpA"
+
 var apikeys = "AIzaSyCwP6lcQH4K_yG3cNK0OI_ziqDqc0ucJIQ"
+const interval = 3000
 
 setInterval(() => {
     update();
-}, 5000);
-
-document.onload(update(ChannelID))
+}, interval);
 
 
 function update() {
     $.getJSON('https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id='+ChannelID+ `&key=${apikeys} `, function(data) {
-        console.log(data)
-
-        document.getElementById("username").innerText = data.items[0].snippet.title
-        document.getElementById("odometer").innerText = data.items[0].statistics.subscriberCount
-        document.getElementById("views").innerText = data.items[0].statistics.viewCount
-        document.getElementById("videos").innerText = data.items[0].statistics.videoCount
-        document.getElementById("pageIcon").innerText = data.items[0].snippet.thumbnails.default.medium
-
-
-
-
+            document.getElementById("username").innerText = data.items[0].snippet.title
+            document.getElementById("odometer").innerText = data.items[0].statistics.subscriberCount
+            document.getElementById("views").innerText = data.items[0].statistics.viewCount
+            document.getElementById("videos").innerText = data.items[0].statistics.videoCount
+            document.getElementById("pageIcon").innerText = data.items[0].snippet.thumbnails.default.medium
     })   
 }
 
