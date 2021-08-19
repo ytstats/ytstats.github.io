d87d65e716f41522647b6413fb7851d33e7807b9
ytstats <nazmulkabir2666@gmail.com>
2021-08-19 09:54:23 +0600

    home page fix

--- assets/scripts/js/base.js
+++ assets/scripts/js/base.js
@@ -1,9 +1,9 @@
   
-var baseURL = "https://NazCounts.cf/youtube";
-if (typeof isCustomPage == 'undefined') isCustomPage = 0;
-if (window.location.hostname.indexOf("127.0.0.1:6969") < 0) {
-    if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
-    if (location.hostname.indexOf("NazCounts.github.io/") == -1) window.location.replace(baseURL + location.hash);
-    if (window.top !== window.self) window.top.location.replace(window.self.location.href);
-}
+// var baseURL = "https://NazCounts.cf/youtube";
+// if (typeof isCustomPage == 'undefined') isCustomPage = 0;
+// if (window.location.hostname.indexOf("127.0.0.1:6969") < 0) {
+//     if (window.location.protocol != "https:") window.location.replace("https:" + window.location.href.substring(window.location.protocol.length));
+//     if (location.hostname.indexOf("NazCounts.github.io/") == -1) window.location.replace(baseURL + location.hash);
+//     if (window.top !== window.self) window.top.location.replace(window.self.location.href);
+// }
 
